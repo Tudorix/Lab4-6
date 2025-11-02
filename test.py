007be8f24fe3f49ebe68d0536543311f6d90f93e
Tudorix <tudorpopescuandrei@gmail.com>
2025-11-02 14:20:48 +0200

    fix the complex get set

--- test.py
+++ test.py
@@ -1,65 +1,96 @@
-import lista
-
-lista_test = lista.ListaComplexe()
+from methods import *
+from complex import *
 
 def test_addAtTheEnd():
     """
         test function for the addAtTheEnd method
     """
-    assert lista_test.addAtTheEnd(lista_curenta={0:[5,0] , 1 : [6,0]} , item=[7,0]) == {0:[5,0] , 1 : [6,0], 2 : [7,0]}
+    reset_list()
+    complex_element = create_complex(7, 8)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    complex_element = create_complex(3,4)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    assert get_list() == [{'real': 7, 'imag': 8}, {'real': 3, 'imag': 4}]
 
 def test_addAtIndex():
     """
         test function for the addAtIndex method
     """
-    assert lista_test.addAtIndex(lista_curenta={0:[5,0] , 1 : [6,0]} , item=[7, 0], index=1) == {0:[5,0] ,1: [7,0] ,2 : [6,0]}
+    reset_list()
+    complex_element = create_complex(7, 8)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    complex_element = create_complex(3,4)
+    set_list(addAtIndex(lista_curenta=get_list(), item=complex_element, index = 0))
+    assert get_list() == [{'real': 3, 'imag': 4}, {'real': 7, 'imag': 8}]
 
 def test_replaceItem():
     """
         test function for the replace method
     """
-    assert lista_test.replaceItem(lista_curenta={0:[5,0] , 1 : [6,0]} , oldItem=[6 , 0], newItem=[20 , 0]) == {0:[5,0] , 1 : [20,0]}
-    assert lista_test.replaceItem(lista_curenta={0:[5,0] , 1 : [6,0], 2 : [6,0]} , oldItem=[6 , 0], newItem=[20 , 0]) == {0:[5,0] , 1 : [20,0], 2 : [20,0]}
-
+    reset_list()
+    complex_element = create_complex(7, 8)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    complex_element = create_complex(3,4)
+    set_list(addAtIndex(lista_curenta=get_list(), item=complex_element, index = 0))
+    complex_element_old = create_complex(3,4)
+    complex_element_new = create_complex(6,6)
+    set_list(replaceItem(lista_curenta=get_list(),oldItem=complex_element_old, newItem=complex_element_new))
+    assert get_list() == [{'real': 6, 'imag': 6}, {'real': 7, 'imag': 8}]
+    
 def test_deleteIndex():
     """
         test function for the deleteAtIndex method
     """
-    assert lista_test.deleteAtIndex(lista_curenta={0:[5,0] , 1 : [6,0]} , index=1) == {0:[5 , 0]} 
+    reset_list()
+    complex_element = create_complex(7, 8)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    complex_element = create_complex(3,4)
+    set_list(addAtIndex(lista_curenta=get_list(), item=complex_element, index = 0))
+    set_list(deleteAtIndex(lista_curenta=get_list(),index = 0))
+    assert get_list() == [{'real': 7, 'imag': 8}]
 
 def test_deleteInterval():
     """
         test function for the deleteInterval method
     """
-    assert lista_test.deleteInterval(lista_curenta={0:[5,0] , 1 : [6,0]}, indexS=0, indexF=1) == {} 
-
-def test_unpackNumebr():
-    """
-        test function for the unpackNumber method
-    """
-    assert lista_test.unpackNumber("5+6i") == [5,6]
-    assert lista_test.unpackNumber("5") == [5,0]
+    reset_list()
+    complex_element = create_complex(7, 8)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    complex_element = create_complex(3,4)
+    set_list(addAtIndex(lista_curenta=get_list(), item=complex_element, index = 0))
+    set_list(deleteInterval(lista_curenta=get_list(), indexS=0,indexF=1))
+    assert get_list() == []
 
 def test_isPrime():
     """
         test function for the isPrime method
     """
-    assert lista_test.isPrime(7)
-    assert lista_test.isPrime(2)
-    assert not lista_test.isPrime(1)
-    assert not lista_test.isPrime(4)
+    assert isPrime(7)
+    assert isPrime(2)
+    assert not isPrime(1)
+    assert not isPrime(4)
 
 def test_filterPrimeNums():
     """
         test function for the filterPrimeNums method
     """
-    assert lista_test.filterPrimeNums(lista_curenta={0: [2,0], 1: [3,0], 2: [5,0], 3: [6,0], 4: [8,0]} , lista=lista_test) == {0: [6,0], 1: [8,0]}
+    reset_list()
+    complex_element = create_complex(7, 8)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    complex_element = create_complex(4,4)
+    set_list(addAtIndex(lista_curenta=get_list(), item=complex_element, index = 0))
+    assert filterPrimeNums(get_list()) == [{'real': 4, 'imag': 4}]
 
 def test_filterCondition():
     """
         test function for the filterCondition method
     """
-    assert lista_test.filterCondition(lista_curenta={0: [2,0]}  , lista=lista_test, case = 1, number = 10) == {}
+    reset_list()
+    complex_element = create_complex(7, 8)
+    set_list(addAtTheEnd(lista_curenta=get_list(), item=complex_element))
+    complex_element = create_complex(10,0)
+    set_list(addAtIndex(lista_curenta=get_list(), item=complex_element, index = 0))
+    assert filterCondition(lista_curenta=get_list(), case=2,number=10) == [{'real': 7, 'imag': 8}]
 
 #Execution Block
 test_filterPrimeNums()
